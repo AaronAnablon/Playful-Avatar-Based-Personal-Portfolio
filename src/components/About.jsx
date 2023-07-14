ee2dfbf94fb384aeca7944cef4bac85b510c647e
AaronAnablon <anablonaaronp@gmail.com>
2023-07-14 21:38:58 +0800

    Added content in the About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,40 @@
-
 import { forwardRef } from 'react';
+import Image from 'next/image';
+import AboutText from '@/helpers/AboutText';
 
 function About({ title }, ref) {
   return (
     <div ref={ref}>
-      <div className="min-h-screen flex align-center justify-center">
-        <h2>{title}</h2>
+      <div className="min-h-screen flex flex-col align-center justify-center">
+        <h2 className='text-6 bg-red-500 text-slate-200 flex justify-center'>{title}</h2>
+        {AboutText.map((text) =>
+          <div className='grid grid-cols-1'>
+            <h2 className='text-4xl'>{text.title}</h2>
+            <div key={text.id} className='grid-cols-1'>
+              <div className='flex h-72'>
+                <div className='rounded-r-full w-1/2 overflow-hidden'>
+                  <Image className='object-fit' src={text.image1} alt={text.title} />
+                </div>
+                <p className='indent-8 w-1/2 mx-6'>{text.paragraph0}</p>
+              </div>
+              <div className='flex h-72'>
+                <p className='indent-8 w-1/2 mx-6'>{text.paragraph2}</p>
+                <div className='rounded-l-full w-1/2 overflow-hidden'>
+                  <Image className='object-fit' src={text.image0} alt={text.title} />
+                </div>
+              </div>
+              <div className='flex h-72'>
+                <div className='rounded-r-full w-1/2 overflow-hidden'>
+                  <Image className='object-fit' src={text.image2} alt={text.title} />
+                </div>
+                <p className='indent-8 w-1/2 mx-6'>{text.paragraph1}</p>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
 }
 
 export default forwardRef(About);
-
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,13 +14,13 @@ function Projects({ title }, ref) {
         <div className="grid md:grid-cols-2 grid-cols-1 gap-4 my-10 z-10">
           {ProjectCards && ProjectCards.map((card, index) => (
             <div
-              className={`h-96 w-1/2 bg-fixed bg-white`}
+              className={`md:w-96 w-30 bg-fixed bg-white`}
               key={card.id}
               onMouseEnter={() => setHoveredCard(index)}
               onMouseLeave={() => setHoveredCard(null)}
             >
               {hoveredCard === index ? (
-                <div className="bg-slate-300 h-full text-slate-600 p-5 grid grid-cols-1 justify-center items-center">
+                <div className="bg-slate-300 text-slate-600 p-5 grid grid-cols-1 justify-center items-center">
                   {card.desc}
                   <div className='flex justify-center gap-10 items-center'>
                     <IoIosOpen size={45}/>
