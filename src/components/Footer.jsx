da7427c04b92dea478ca6c8ca85b459d1e8dd4ec
AaronAnablon <anablonaaronp@gmail.com>
2023-08-01 18:17:24 +0800

    Added target

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -28,9 +28,9 @@ const Footer = ({ setViewPort }) => {
                 </div>
                 <div className='flex gap-4 justify-center items-center'>
                    <button onClick={handleSendEmail}><IoIosMail size={38} /></button> 
-                   <Link href='https://github.com/AaronAnablon'><AiFillGithub size={34} /></Link>  
-                   <Link href='https://www.facebook.com/techibot'><BsFacebook size={28} /></Link>   
-                   <Link href='https://www.linkedin.com/in/aaronpanablon'><BsLinkedin size={28} /></Link>   
+                   <Link href='https://github.com/AaronAnablon' target='_blank'><AiFillGithub size={34} /></Link>  
+                   <Link href='https://www.facebook.com/techibot' target='_blank'><BsFacebook size={28} /></Link>   
+                   <Link href='https://www.linkedin.com/in/aaronpanablon' target='_blank'><BsLinkedin size={28} /></Link>   
                 </div>
                 <div className='flex justify-center'>
                     <Image width={100} src={me} alt='me' />
